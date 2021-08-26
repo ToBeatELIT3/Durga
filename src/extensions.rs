ab0609759d20753b716399532c67f73b1bf337ce
FalseGhost <amitojsingh8070@gmail.com>
2021-08-26 15:09:42 -0400

    temp extension

--- src/extensions.rs
+++ src/extensions.rs
@@ -3,10 +3,10 @@
 pub fn run_extensions(open_port: u16) {
     match open_port {
         22 => {
-            println!("hi ssh");        
+            ();        
         },
         80 => {
-            println!("web server ooh");
+            ();
         }
         _ => ()
         
