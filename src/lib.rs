a8146251c7d1871b59e734274d2bf57a897cb01c
FalseGhost <amitojsingh8070@gmail.com>
2021-09-06 11:09:44 -0400

    command execution prototype

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,25 @@ use tokio::net::TcpStream;
 use futures::stream;
 use futures::StreamExt;
 
+use std::ffi::CString;
+use std::os::raw::c_char;
+
 mod extensions;
+mod ports;
+
+pub struct CommandModule {
+    title: String,
+    command_exec: String,
+}
+
+pub fn run_command(my_command: &str) {
+    extern "C" { fn system(my_command: *const c_char) -> *const c_char; }
+    let cstring_command = CString::new(my_command).expect("CString::new failed");
+
+    unsafe {
+        system(cstring_command.as_ptr());
+    }
+}
 
 pub fn resolve_target(target: &String) -> Result<IpAddr, Box<dyn std::error::Error>> {
     match target.parse() {
@@ -22,15 +40,15 @@ pub fn resolve_target(target: &String) -> Result<IpAddr, Box<dyn std::error::Err
     }
 }
 
-pub async fn scan(target: IpAddr, full: bool, concurrency: usize, timeout: Duration) {
+pub async fn scan(target: IpAddr, full: bool, concurrency: usize, timeout: Duration, target_name: &str) {
     let ports = stream::iter(get_ports(full));
 
     ports
-        .for_each_concurrent(concurrency, |port| scan_port(target, port, timeout, full))
+        .for_each_concurrent(concurrency, |port| scan_port(target, port, timeout, full, target_name))
         .await;
 }
 
-async fn scan_port(target: IpAddr, current_port: u16, timeout: Duration, full: bool) {
+async fn scan_port(target: IpAddr, current_port: u16, timeout: Duration, full: bool, target_name: &str) {
     let socket_address = SocketAddr::new(target.clone(), current_port);
     let connection_status = tokio::time::timeout(timeout, TcpStream::connect(&socket_address))
     .await;
@@ -46,9 +64,9 @@ async fn scan_port(target: IpAddr, current_port: u16, timeout: Duration, full: b
             match current_stream_wrapper {
 
                 Ok(_current_stream) => {
-
+                                 
+                    run_command(format!("echo {} >> /tmp/{}.txt", current_port, target_name).as_str());
                     println!{"[*] OPEN PORT {}", current_port};
-                    extensions::run_extensions(current_port);
                 }
                 _ => ()   
             }
@@ -61,7 +79,7 @@ fn get_ports(all_ports: bool) -> Box<dyn Iterator<Item = u16>> {
     if all_ports {
         Box::new((1..=u16::MAX).into_iter())
     } else {
-        Box::new(extensions::COMMON_PORTS.to_owned().into_iter())
+        Box::new(ports::COMMON_PORTS.to_owned().into_iter())
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
 // FalseGhost
 
-use durga::{self, banner};
-use std::time::Duration;
-
-use tokio;
 #[macro_use]
 extern crate clap;
 use clap::App;
 
+use tokio;
+use durga;
+
 mod extensions;
+
 #[tokio::main]
 async fn main() {
 
-    banner();
+    durga::banner();
     let yaml = load_yaml!("../resources/cli.yaml");
     let argv = App::from_yaml(yaml).get_matches();
     
     let unresolved_target = argv.value_of("target_ip").unwrap();
- 
+    let timeout = std::time::Duration::from_secs(1);
+    
     let my_target = durga::resolve_target(&unresolved_target.to_string());
     println!("[*] Scanning {} -> {}", argv.value_of("target_ip").unwrap(), durga::resolve_target(&unresolved_target.to_string()).unwrap());
 
     
     match argv.occurrences_of("full_scan") {
         0 => {
-            durga::scan(my_target.unwrap(), false, extensions::COMMON_PORTS.len(), Duration::from_secs(1))
+            durga::scan(my_target.unwrap(), false, 1024, timeout, unresolved_target)
                 .await;
         },
         1 => {
             println!("[*] Running Full TCP Scan");
-            durga::scan(my_target.unwrap(), true, 1000, Duration::from_secs(1))
+            durga::scan(my_target.unwrap(), true, 1024, timeout, unresolved_target)
                 .await;
         }
         _ => ()
     }
-}
 
+    let open_ports_list = std::fs::read_to_string(format!("/tmp/{}.txt", unresolved_target)).unwrap();
+ 
+    for line in open_ports_list.lines() {
+        extensions::run_extensions(line.parse::<u16>().unwrap(), unresolved_target);
+    }
+
+    durga::run_command(format!("rm /tmp/{}.txt", unresolved_target).as_str());   
+}
